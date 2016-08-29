6c8d7803de4068789b492182ed3f8bdd48e93768
Petar Ploshtakov <p.a.ploshtakov@gmail.com>
2016-08-29 19:17:54 +0300

    register page xml

--- app/src/main/java/com/example/pesho/make_a_coctail/LoginActivity.java
+++ app/src/main/java/com/example/pesho/make_a_coctail/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.pesho.make_a_coctail;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button register = (Button) findViewById(R.id.LPRegisterButton);
+        Button login = (Button) findViewById(R.id.LPLoginButton);
+        Button regByFace = (Button) findViewById(R.id.LPFacebookLoginButton);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(reg);
+            }
+        });
     }
 }
