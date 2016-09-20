a500730c4f6d1334cc7e4ddcbf48ea6b7c763ac0
Simeon Angelov <simeon.angelov9@gmail.com>
2016-09-20 23:27:30 +0300

    fix multiple add of products

--- app/src/main/java/com/example/pesho/make_a_cocktail/model/products/Product.java
+++ app/src/main/java/com/example/pesho/make_a_cocktail/model/products/Product.java
@@ -26,4 +26,20 @@ public  class Product {
         this.image = image;
         this.isInShoppingList = isInShoppingList;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return name != null ? name.equals(product.name) : product.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
